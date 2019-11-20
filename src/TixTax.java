727036a7d921e89cc9db70779aba5b5b8d9846d1
undertrox <undertrox@gmail.com>
2019-11-20 16:10:52 +0100

    Added WeightedAI

--- src/TixTax.java
+++ src/TixTax.java
@@ -1,15 +1,19 @@
 import de.undertrox.jtixtax.Game;
 import de.undertrox.jtixtax.Player;
-import de.undertrox.jtixtax.ai.CommandLinePlayer;
-import de.undertrox.jtixtax.ai.RandomAI;
+import de.undertrox.jtixtax.ai.*;
 
 public class TixTax {
     public static void main(String[] args) {
-        Player p1 = new RandomAI("RandomAI");
-        Player p2 = new CommandLinePlayer("Name");
+        Player p1 = new WeightedAI("RandomAI",
+                                   new FirstFieldAI(""), 50,
+                                   new LastFieldAI(""), 50);
+        Player p2 = new WeightedAI("Name",
+                                   new RandomAI(""), 90,
+                                   new CommandLinePlayer(""), 10);
         Game g = new Game(p1, p2);
         while (!g.hasEnded()) {
             g.playOneTurn();
+            System.out.println(g.draw());
         }
         System.out.println(g.status());
     }
---
+++ src/de/undertrox/jtixtax/ai/WeightedAI.java
@@ -0,0 +1,39 @@
+package de.undertrox.jtixtax.ai;
+
+import de.undertrox.jtixtax.Box;
+import de.undertrox.jtixtax.Game;
+import de.undertrox.jtixtax.Player;
+
+import java.util.Random;
+
+public class WeightedAI extends Player {
+    private Player p1;
+    private Player p2;
+    private int p1Weight;
+    private int p2Weight;
+    private static Random r = new Random();
+
+    public WeightedAI(String name, Player p1, int p1Weight, Player p2, int p2Weight) {
+        super(name);
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p1Weight = p1Weight;
+        this.p2Weight = p2Weight;
+    }
+
+    public void init(Box color, Game parentGame) {
+        super.init(color, parentGame);
+        p1.init(color, parentGame);
+        p2.init(color, parentGame);
+    }
+
+    @Override
+    public int[] play(Game game) {
+        int totalWeight = p1Weight + p2Weight;
+        if (r.nextInt(totalWeight) < p1Weight) {
+            return p1.play(game);
+        } else {
+            return p2.play(game);
+        }
+    }
+}
