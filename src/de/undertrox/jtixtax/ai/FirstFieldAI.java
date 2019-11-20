411feef9d097fd0cd9e54572f846e8b02b30c7bf
undertrox <undertrox@gmail.com>
2019-11-20 15:56:02 +0100

    Added FirstFieldAI

---
+++ src/de/undertrox/jtixtax/ai/FirstFieldAI.java
@@ -0,0 +1,35 @@
+package de.undertrox.jtixtax.ai;
+
+import de.undertrox.jtixtax.Game;
+import de.undertrox.jtixtax.Player;
+
+public class FirstFieldAI extends Player {
+
+    public FirstFieldAI(String name) {
+        super(name);
+    }
+
+    @Override
+    public int[] play(Game game) {
+        int bigRow = 0, bigCol = 0, smallRow = 0, smallCol = 0;
+        while (!canSet(bigRow, bigCol, smallRow, smallCol)) {
+            smallRow++;
+            if (smallRow>2) {
+                smallRow = 0;
+                smallCol++;
+                if (smallCol>2) {
+                    smallCol = 0;
+                    bigRow++;
+                    if (bigRow >2){
+                        bigRow = 0;
+                        bigCol++;
+                        if (bigCol>2) {
+                            bigCol = 0;
+                        }
+                    }
+                }
+            }
+        };
+        return new int[]{bigRow, bigCol, smallRow, smallCol};
+    }
+}
