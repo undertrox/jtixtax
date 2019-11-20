4b7660ef96a2360526b031d96db1ec428cba94fd
undertrox <undertrox@gmail.com>
2019-11-20 13:09:40 +0100

    cleaned up TixTax.java

--- src/TixTax.java
+++ src/TixTax.java
@@ -3,17 +3,13 @@ import de.undertrox.jtixtax.Player;
 import de.undertrox.jtixtax.ai.CommandLinePlayer;
 import de.undertrox.jtixtax.ai.RandomAI;
 
-import java.io.IOException;
-
 public class TixTax {
     public static void main(String[] args) {
-        int r1 = 0, r2 = 0, tie = 0;
         Player p1 = new RandomAI("RandomAI");
-        Player p2 = new CommandLinePlayer("Kevin");
+        Player p2 = new CommandLinePlayer("Name");
         Game g = new Game(p1, p2);
         while (!g.hasEnded()) {
             g.playOneTurn();
-            System.out.println(g.draw());
         }
         System.out.println(g.status());
     }
