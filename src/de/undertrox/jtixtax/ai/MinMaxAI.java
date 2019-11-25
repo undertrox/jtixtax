938b0b5532aa7ffcac32ef94fbdd0e78c62010f9
undertrox <undertrox@gmail.com>
2019-11-25 11:49:18 +0100

    MinMaxAI now looks 1 step ahead

--- src/de/undertrox/jtixtax/ai/MinMaxAI.java
+++ src/de/undertrox/jtixtax/ai/MinMaxAI.java
@@ -1,6 +1,7 @@
 package de.undertrox.jtixtax.ai;
 
 import de.undertrox.jtixtax.game.Box;
+import de.undertrox.jtixtax.game.Game;
 import de.undertrox.jtixtax.game.Move;
 import de.undertrox.jtixtax.game.state.GameState;
 
@@ -14,25 +15,50 @@ public class MinMaxAI extends Player{
 
     @Override
     public Move play(GameState gameState) {
-        return null;
+        List<GameState> states = new ArrayList<>();
+        for (Move m : gameState.getValidMoves()) {
+            states.add(gameState.nextState(m));
+        }
+        List<Integer> scores = new ArrayList<>();
+        for (GameState s : states) {
+            scores.add(getScore(s));
+        }
+        int maxIndex =-1;
+        int maxVal = -1000;
+        for (int i = 0; i<scores.size(); i++) {
+            if (maxVal < scores.get(i)) {
+                maxIndex = i;
+                maxVal = scores.get(i);
+            }
+        }
+        return gameState.getValidMoves().get(maxIndex);
     }
 
     public int getScore(GameState state) {
         int score = 0;
+        Box winner = state.getWinner();
+        if (winner == getColor()) {
+            return 1000;
+        } else if (winner == getColor().getOpposite()) {
+            return -1000;
+        }
         List<Move> ownCells = new ArrayList<>();
         List<Move> opponentCells = new ArrayList<>();
         List<Move> fullCells = new ArrayList<>();
-        List<Move> emptyCels = new ArrayList<>();
+        List<Move> emptyCells = new ArrayList<>();
         for (int i = 0; i<3; i++) {
             for (int j = 0; j <3; j++) {
                 if (state.getCell(i,j).getTotalState() == getColor()) {
                     ownCells.add(new Move(i,j));
+                    score += 100;
                 } else if (state.getCell(i,j).getTotalState() == getColor().getOpposite()) {
                     opponentCells.add(new Move(i,j));
+                    score -= 100;
                 } else if (state.getCell(i,j).getTotalState() == Box.FULL){
                     fullCells.add(new Move(i,j));
+                    score -= 20;
                 } else {
-                    emptyCels.add(new Move(i,j));
+                    emptyCells.add(new Move(i,j));
                 }
             }
         }
